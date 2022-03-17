f4d529043605e0df68221e044a9f1abe0e6a01be
Hunter Kelly <47648040+HunterKelly@users.noreply.github.com>
2022-03-17 12:05:53 -0500

    Basic Java submissiojn

--- Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
@@ -1,1 +1,22 @@
 window.alert("Hello, World!");
+document.write('Hello World!');
+var x = " This is a string";
+document.write(x);
+document.write(" Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded");
+document.write(" \"Be who you are and say what you feel,"
+    + " because those who mind don\'t matter and those who matter don\'t mind\""
+    + "-Dr Seuss");
+var B = " Concatenated" + " String"
+document.write(B);
+
+var Family = " The Arezzinis", Dad = " Jeremiah", Mom = " Hermoine", Daughter = " Penny", Son = " Zorro";
+document.write(Dad);
+document.write(3 + 3);
+
+function My_First_Function() {    //Defining a function and naming it
+    var str = "This is the button text!"; //Defining a variable and giving it
+                                            //a string value
+    document.getElementById("Button_Text").innerHTML = str; //Putting the value
+                                            //of the variable into the HTML elementFromPoint
+                                            //with the "Button_Text" id
+}
