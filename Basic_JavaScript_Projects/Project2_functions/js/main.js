d6fe85a49f4d209920b7ed9f06ed189e4be911bc
Hunter Kelly <47648040+HunterKelly@users.noreply.github.com>
2022-03-18 09:37:59 -0500

    comments

--- Basic_JavaScript_Projects/Project2_functions/js/main.js
+++ Basic_JavaScript_Projects/Project2_functions/js/main.js
@@ -1,16 +1,17 @@
-var x = "Father";
-var y = "Mother";
+var x = "Father"; //unused variables for whatever
+var y = "Mother"; //
 
 function button1() {
-    var button1 = "BOO!"
-    document.getElementById("buttonText").innerHTML = button1
+    var button1 = "BOO!" //stored variable to be used in the function
+    document.getElementById("buttonText").innerHTML = button1 //changes text to stored variable
 }
 
-var Birds = "Flock", Dogs = "Pack", Cows = "herd", Ants = "colony";
+var Birds = "Flock", Dogs = "Pack", Cows = "herd", Ants = "colony"; //unused variables for whatever
 
 function para() {
-    var text1 = "Here is additional text! "
-    text1 += "And even MORE TEXT! "
-    text1 += "Muahahahaha!"
-    document.getElementById("para1").innerHTML = text1
+    var text1 = "Here is additional text! " //The text in HTML changes into this on click
+    text1 += "And even MORE TEXT! "         //This text is concatonated into the above
+    text1 += "Muahahahaha!"                 //concatonated as well
+    document.getElementById("para1").innerHTML = text1 //this inputs the text into the html
+                                                       //(the actual function part of the function)
 }
