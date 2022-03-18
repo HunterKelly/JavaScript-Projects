5d8786b1864f96575a6dc4601e4ba9ced9c0d063
Hunter Kelly <47648040+HunterKelly@users.noreply.github.com>
2022-03-18 09:33:40 -0500

    Lets goo!

---
+++ Basic_JavaScript_Projects/Project2_functions/js/main.js
@@ -0,0 +1,16 @@
+var x = "Father";
+var y = "Mother";
+
+function button1() {
+    var button1 = "BOO!"
+    document.getElementById("buttonText").innerHTML = button1
+}
+
+var Birds = "Flock", Dogs = "Pack", Cows = "herd", Ants = "colony";
+
+function para() {
+    var text1 = "Here is additional text! "
+    text1 += "And even MORE TEXT! "
+    text1 += "Muahahahaha!"
+    document.getElementById("para1").innerHTML = text1
+}
