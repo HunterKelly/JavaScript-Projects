1e1a1c26b05d278d7fb414e40dd5a32e0c75aa3c
Hunter Kelly <47648040+HunterKelly@users.noreply.github.com>
2022-03-21 13:08:01 -0500

    project5

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,6 +1,9 @@
-document.write(typeof "Word");
-document.write(typeof 3);
-document.write("10" + 5);
+document.write(typeof "Word" + " ");
+
+document.write(typeof 3 + " ");
+
+document.write("10" + 5 + " ");
+
 
 function my_Function() {
     document.getElementById("Test").innerHTML = 0 / 0;
@@ -14,5 +17,52 @@ function my_Functionnn() {
     document.getElementById("Test3").innerHTML = isNaN('007');
 }
 
-document.write(2E310);
-document.write(-3E310);
+document.write(2E310 + " ");
+
+document.write(-3E310 + " ");
+
+document.write(10 > 2 + "  ");
+
+document.write(10 < 2 + "  ");
+
+console.log(2 + 2);
+
+console.log(5 < 2);
+
+document.write(10 == 10 + " ");
+
+document.write(3 == 11 + " ");
+
+var DateOfBirth1 = "1/1/1970";
+var DateOfBirth2 = "1/1/1970";
+
+document.write(DateOfBirth1 === DateOfBirth2);
+
+X = 82;
+Y = "82";
+
+document.write(X === Y);
+
+A = 82;
+B = "Person";
+document.write(A === B);
+
+C = 82;
+D = 85;
+
+document.write(C == D);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function() {
+    document.getElementById("Test4").innerHTML = !(20 > 10);
+}
+function notnot_Function() {
+    document.getElementById("Test5").innerHTML = !(5 > 10);
+}
---
+++ Basic_JavaScript_Projects/Project6/js/main.js
@@ -0,0 +1,3 @@
+document.write(10 > 2);
+
+document.write(10 < 2);
