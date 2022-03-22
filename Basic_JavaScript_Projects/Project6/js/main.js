9a33bae93105cad30021234eda5b1d0c66ef0bf0
Hunter Kelly <47648040+HunterKelly@users.noreply.github.com>
2022-03-22 09:13:11 -0500

    submission

--- Basic_JavaScript_Projects/Project6/js/main.js
+++
@@ -1,3 +0,0 @@
-document.write(10 > 2);
-
-document.write(10 < 2);
---
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructors/js/main.js
@@ -0,0 +1,46 @@
+document.write(Bigger = (5 > 1) ? "Left number is bigger" : "Right number is bigger");
+
+function Ride_Function() {
+    var Height, Can_Ride;
+    Height = document.getElementById("Height").value;
+    Can_Ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_Ride + " to ride";
+
+}
+
+function Vote_Function() {
+    var Age, Can_Vote;
+    Age = document.getElementById("Age").value;
+    Can_Vote = (Age < 18) ? "You are too young" : "You are old enough";
+    document.getElementById("Vote").innerHTML = Can_Vote + " to vote."
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function keyFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+        "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
+}
+function lock_Function() {
+    shutItDown = "Nuclear Launch Detected!"
+    document.getElementById("New_and_This").innerHTML = shutItDown
+}
+function cheat_Function() {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() { Starting_point += 1; }
+        Plus_one();
+        return Starting_point;
+
+    }
+}
+
